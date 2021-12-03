ab74fdc369182197432c66850aabd517bf4eb9fc
TESLA2402 <83589511+TESLA2402@users.noreply.github.com>
2021-12-03 16:41:33 +0530

    Update LoginPage.dart

--- lib/Screens/LoginPage.dart
+++ lib/Screens/LoginPage.dart
@@ -116,6 +116,24 @@ class _SignInState extends State<SignIn> {
         title: Text("Sign In"),
         backgroundColor: Colors.pink[400],
         elevation: 0,
+        actions: [
+           FlatButton.icon(
+               onPressed: () {
+                 widget.toggleView();
+               },
+               icon: Icon(
+                   Icons.person,
+                 color: Colors.white,
+               ),
+               label: Text(
+                 'Register',
+                 style: TextStyle(
+                   color: Colors.white,
+                 ),
+               )
+           )
+         ],
+
         
       ),
       body: Container(
