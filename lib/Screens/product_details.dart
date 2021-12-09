c8690f1061ee8379af93fef10128fd120ea641d3
Starkl7 <dhrubojeet17@gmail.com>
2021-12-10 01:28:12 +0530

    Fixed unused import

--- lib/Screens/product_details.dart
+++ lib/Screens/product_details.dart
@@ -1,5 +1,5 @@
 import 'package:carousel_pro/carousel_pro.dart';
-import 'package:flutter/cupertino.dart';
+//import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:test_app/Screens/Loading.dart';
